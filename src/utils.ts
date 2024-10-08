05b7519cdd920c5ddb7c8a03896096054c72a2cc
Patrick Connolly <patrick.c.connolly@gmail.com>
2024-10-08 16:22:19 -0400

    Allow action to work on pull_request_target triggers.

--- src/utils.ts
+++ src/utils.ts
@@ -3,6 +3,7 @@ import * as github from '@actions/github'
 export async function validateEventType(): Promise<void> {
   if (
     github.context.eventName !== 'pull_request' &&
+    github.context.eventName !== 'pull_request_target' &&
     github.context.eventName !== 'push'
   ) {
     throw new Error('Error, action only works for pull_request or push events!')
