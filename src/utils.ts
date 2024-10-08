ecb33b7b260c3bd5e3181fc3e3389635536d1ace
Patrick Connolly <patrick.c.connolly@gmail.com>
2024-10-08 16:47:05 -0400

    Updated README and error output.

--- src/utils.ts
+++ src/utils.ts
@@ -6,6 +6,8 @@ export async function validateEventType(): Promise<void> {
     github.context.eventName !== 'pull_request_target' &&
     github.context.eventName !== 'push'
   ) {
-    throw new Error('Error, action only works for pull_request or push events!')
+    throw new Error(
+      'Error, action only works for pull_request, pull_request_target or push events!'
+    )
   }
 }
